39907e1da284c2c51527161a0c056df036605f2b
YOUR NAME <YOUR@EMAIL.com>
2023-11-29 13:22:58 +0300

    complete lab

--- lib/dog.py
+++ lib/dog.py
@@ -12,4 +12,14 @@ APPROVED_BREEDS = [
 ]
 
 class Dog:
-    pass
+   def __init__(self, name):
+     self.name =None
+     def get_name(self):
+       return self._name
+   def set_name(self, name):
+    if (type(name) in (str)) and (1 <= len(name) <= 25):
+      self._name = name
+    else:
+      print("Name must be string between 1 and 25 characters.")
+fido = Dog()
+pass
